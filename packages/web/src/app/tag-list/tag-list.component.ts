139d41d54dadcffdcb46f4a54f42b5618e2e504f
LoTwT <709937065@qq.com>
2022-04-25 14:11:09 +0800

    feat(web): tag-list style

--- packages/web/src/app/tag-list/tag-list.component.ts
+++ packages/web/src/app/tag-list/tag-list.component.ts
@@ -6,7 +6,71 @@ import { Component, OnInit } from "@angular/core"
   styleUrls: ["./tag-list.component.less"],
 })
 export class TagListComponent implements OnInit {
+  tagList: ITag[] = [
+    {
+      icon: "home",
+      name: "计算机",
+      sum: 2,
+    },
+    {
+      icon: "menu",
+      name: "菜单",
+      sum: 21,
+    },
+    {
+      icon: "list",
+      name: "列表",
+      sum: 12,
+    },
+    {
+      icon: "home",
+      name: "web 开发",
+      sum: 25,
+    },
+    {
+      icon: "home",
+      name: "机器学习",
+      sum: 0,
+    },
+    {
+      icon: "home",
+      name: "Markdown",
+      sum: 11,
+    },
+    {
+      icon: "home",
+      name: "计算机",
+      sum: 2,
+    },
+    {
+      icon: "menu",
+      name: "菜单",
+      sum: 21,
+    },
+    {
+      icon: "list",
+      name: "列表",
+      sum: 12,
+    },
+    {
+      icon: "home",
+      name: "web 开发",
+      sum: 25,
+    },
+    {
+      icon: "home",
+      name: "机器学习",
+      sum: 0,
+    },
+  ]
+
   constructor() {}
 
   ngOnInit(): void {}
+}
+
+interface ITag {
+  icon: string
+  name: string
+  sum: number
 }
