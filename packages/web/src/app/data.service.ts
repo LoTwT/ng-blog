9fd664c8b33cb7c8607c52c32d551392c2744dfa
LoTwT <709937065@qq.com>
2022-04-26 14:02:59 +0800

    chore: add data.service

---
+++ packages/web/src/app/data.service.ts
@@ -0,0 +1,12 @@
+import { HttpClient } from "@angular/common/http"
+import { Injectable } from "@angular/core"
+import { IRankList } from "@ng-blog/shared-types"
+
+@Injectable({
+  providedIn: "root",
+})
+export class DataService {
+  constructor(private http: HttpClient) {}
+
+  getHotArticles = () => this.http.get<IRankList>("api/hot")
+}
